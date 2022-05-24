8c9750b4284a5a7fdd38fff3d4e4684b33eb2fa6
LOVE-DOCTOR <18-52ha137@gmail.com>
2022-05-24 16:47:56 +0100

    Fixed bug and added baking

--- Food/Cake.py
+++ Food/Cake.py
@@ -2,15 +2,29 @@ import time
 import random
 
 
+# The class Cake has a constructor that takes two arguments, ingredients and home_made_cake.
+# The constructor initializes the instance variables ingredients and home_made_cake.
+# The instance variable ingredients is initialized to an empty list if no argument is passed to the constructor.
+# The instance variable home_made_cake is initialized to False if no argument is passed to the constructor.
+# The class also has two instance variables, messagesList and messagesList2, which are initialized to list of strings.
+# The class also has an instance variable successMessage, which is initialized to a list of strings.
+# The class also has an instance variable numbers, which is initialized to a list of integers.
 class Cake:
-    def __init__(self, tries: int, ingredients=None, home_made_cake=False):
+    def __init__(self, ingredients=None, home_made_cake=False):
+        """
+        The function __init__ is a constructor that initializes the class Cake with the attributes ingredients,
+        home_made_cake, messagesList, messagesList2, successMessage, and numbers
+
+        :param ingredients: a list of ingredients that the player has to use to make the cake
+        :param home_made_cake: a boolean that is True if the cake is homemade and False if it is not, defaults to False
+        (optional)
+        """
         if ingredients is None:
             ingredients = []
         self.ingredients = ingredients
         self.home_made_cake = home_made_cake
-        self.tries = tries
         self.messagesList = ["You are a culinary disaster and should not be allowed near the stove.",
-                             "I know you're trying to make a homemade cake, but your recipe is turning into a disaster.",
+                             "I know you're trying to make a homemade cake, but your recipe is turning a disaster.",
                              "I don't know who told you to try baking, maybe you'd be better off doing the eating.",
                              "That is one way to ruin a cake.",
                              " If you bake a cake that way, you will end up with something more resembling a muffin."]
@@ -23,34 +37,49 @@ class Cake:
         self.numbers = [1, 2, 3, 4]
 
 
-def Print(message: (str, int, float)) -> None:
+def Print(message: any) -> None:
+    """
+    Prints a message to the screen and waits 2 seconds before continuing
+
+    :param message: The message to be printed
+    :type message: any
+    """
     print(message)
     time.sleep(2)
 
 
+# We are baking a cake, and we need to follow the instructions to bake it
 class HomeMadeCake(Cake):
     def __init__(self):
-        Cake.__init__(self, 2, ["flour", "egg", "baking soda", "vanilla essence", "powdered sugar", "butter", "milk"],
+        """
+        The function __init__() is a constructor that initializes the attributes of the class Cake
+        """
+        Cake.__init__(self, ["flour", "egg", "baking soda", "vanilla essence", "powdered sugar", "butter", "milk"],
                       home_made_cake=True)
 
-    def CheckSuccess(self, correct_option):
+    # A function that checks if the user has selected the correct option.
+    def CheckSuccess(self, correct_option: int) -> None:
+
         while True:
-            doFirstStep = int(input("Select 1, 2 or 3: "))
-            if doFirstStep in self.numbers:
-                if doFirstStep != correct_option:
-                    print(random.choice(self.messagesList))
-                    self.tries -= 1
-                    if self.tries == 0:
-                        print(random.choice(self.messagesList2))
-                        break
-                elif doFirstStep == correct_option:
-                    self.tries = 2
+
+            try:
+                doFirstStep = int(input("Select 1, 2 or 3: "))
+
+                if doFirstStep == correct_option:
                     Print(random.choice(self.successMessage))
                     break
-            else:
-                print("Option not in steps")
 
-    def listIngredients(self):
+                elif doFirstStep != correct_option:
+                    Print(random.choice(self.messagesList))
+                    Print("Try again!")
+
+            except ValueError:
+                print("Please select from the available options.")
+
+    def listIngredients(self) -> None:
+        """
+        This function prints out the ingredients of the cake
+        """
         Print("We will be baking our HomeMadeCake with the following ingredients")
         Print(f"1. 3 cups of {self.ingredients[0]}")
         Print(f"2. 4 pieces of {self.ingredients[1]}")
@@ -60,7 +89,10 @@ class HomeMadeCake(Cake):
         Print(f"6. 1 cup of {self.ingredients[5]}")
         Print(f"7. 1 cup of {self.ingredients[6]}")
 
-    def mixIngredients(self):
+    def mixIngredients(self) -> None:
+        """
+        The function mixes the ingredients together and asks the user to choose the correct steps to mix the ingredients
+        """
         self.listIngredients()
 
         Print("It's time to mix the ingredients together")
--- oven.py
+++ oven.py
@@ -1,25 +1,33 @@
 import time
-from Food.Cake import HomeMadeCake
+from Food.Cake import HomeMadeCake, Print
 
 
 class Oven:
 
-    def __init__(self, temperature=0, timer=0, state=False):
+    def __init__(self, temperature: int = 0, timer: int = 0, state: bool = False):
+        """
+        This function initializes the class with the default values of temperature, timer, and state.
+
+        :param temperature: The temperature of the oven, defaults to 0 (optional)
+        :param timer: The amount of time the oven should be on for, defaults to 0 (optional)
+        :param state: True or False, defaults to False (optional)
+        """
         self.temperature = temperature
         self.timer = timer
         self.state = state
 
-    def putOvenOn(self):
+    def putOvenOn(self) -> bool:
+        # Asking the user if they want to put on the oven.
         selectState = ["y", "n"]
         print("Do you want to put on the oven? y/n")
         while True:
             switchState = input("Here: ").lower()
             if switchState == "y":
                 self.state = True
-                print("Starting oven...")
-                time.sleep(3)
-                print("Oven is now on")
+                Print("Starting oven...")
+                Print("Oven is now on")
                 break
+            # It's setting the state to the current state.
             elif switchState == "n":
                 self.state = self.state
                 print("Come back when you have something to bake.")
@@ -30,31 +38,60 @@ class Oven:
 
         return self.state
 
-    def startMix(self):
-        state = self.putOvenOn()
-        if state is True:
-            print("What do you want to bake?")
-            while True:
-                try:
-                    print("1. Cake")
-                    foodToBake = int(input("Here: "))
-                    break
-                except ValueError:
-                    print("Please select from the options you're given.")
+    def startMix(self) -> int:
+        Print("What do you want to bake?")
+        while True:
+            try:
+                Print("1. Cake")
+                foodToBake = int(input("Here: "))
+                break
+            except ValueError:
+                Print("Please select from the options you're given.")
 
-            if foodToBake == 1:
-                print("What kind of cake would you like to bake?")
-                print("1. Homemade Cake")
-                homemadeCake = int(input("Here: "))
-                if homemadeCake == 1:
-                    cake = HomeMadeCake()
-                    cake.mixIngredients()
-            else:
-                print("Option not available yet")
+        if foodToBake == 1:
+            Print("What kind of cake would you like to bake?")
+            Print("1. Homemade Cake")
+            cakeType = int(input("Here: "))
+            if cakeType == 1:
+                cake = HomeMadeCake()
+                cake.mixIngredients()
+                state = self.putOvenOn()
+                if state is False:
+                    print("Oven is not on.")
+                    time.sleep(1)
+                    print("Sleeping...")
+                else:
+                    pass
+            return cakeType
         else:
-            print("Oven is not on.")
-            time.sleep(1)
-            print("Sleeping...")
+            print("Option not available yet")
+
+    def startBaking(self) -> None:
+        Print("To start baking, you have to set the desired temperature of the oven")
+        Print("Remember to set an optimal temperature to avoid under-baking or burning your food.")
+
+        while True:
+            try:
+                temperature_type = int(input("Set temperature unit to 1. Celsius, 2. Fahrenheit, 3. Kelvin"))
+
+                if temperature_type == 1 or temperature_type == 2 or temperature_type == 3:
+                    break
+                else:
+                    print("Please select a valid unit of temperature.")
+
+            except ValueError:
+                print("Please select from the options above")
+
+        while True:
+            try:
+                if temperature_type == 1:
+                    self.temperature = int(input("Set temperature here(in °C) 0°C - 250°C: "))
+
+
+            except ValueError:
+                print("Please select from the options above")
+
+        Print(f"You have now successfully set the temperature of the oven to {self.temperature}°C")
 
 
 if __name__ == '__main__':
